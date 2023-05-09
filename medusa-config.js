931ec6677b9ae48771ccd5844eca0083e957b6f7
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-09 16:22:29 +0200

    chore: update medusa config for heroku

--- medusa-config.js
+++ medusa-config.js
@@ -80,7 +80,16 @@ if (DATABASE_URL && DATABASE_TYPE === "postgres") {
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
-  projectConfig,
+  projectConfig: {
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
+  },
   plugins,
-  modules,
 };
