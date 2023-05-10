3ff26ce3ef47a48af9da011a6b6b469ba5e65b4b
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-10 13:19:20 +0200

    chore: add build folder

---
+++ build/assets/404-1a1083dd.js
@@ -0,0 +1,1 @@
+import{j as o,a as t}from"./index-5618be5b.js";import{S as s}from"./index-c4aaed71.js";import{L as a}from"./layout-efb90978.js";import"./index-8aa3005c.js";const n=()=>o(a,{children:[t(s,{title:"404: Not found"}),t("h1",{children:"NOT FOUND"}),t("p",{children:"You just hit a route that doesn't exist... the sadness."})]});export{n as default};
